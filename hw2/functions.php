32815c0e7af90c159c54e7d4cfd03a41f4ac4902
Interior <a2378066@yandex.ru>
2017-10-25 14:14:29 +0300

    Initial commit

---
+++ hw2/functions.php
@@ -0,0 +1,218 @@
+﻿<?php
+
+function task1($arr, $b = false) {
+
+if ($b == true) { 
+$arr = print_r(implode($arr,""));
+return $arr;
+
+} else {
+    foreach ($arr as $str) {
+        echo '<p>' . $str . '</p>';
+        }
+   }
+
+};
+//////////////////////////////////////////////////////////
+function task2($nums,$math){
+    foreach($nums as $num) {
+        if (!is_numeric($num)) {
+            echo '<b>Результат: нужны только цифры нам, о юный падаван!</b>';
+            die();
+        }
+    };
+            switch ($math) {
+
+                case '+':
+                    $sum = 0;
+                    foreach ($nums as $num) {
+                        $sum += $num;
+                    };
+                    break;
+                case '-':
+                    $sum = $nums[0];
+                    foreach ($nums as $key => $num) {
+                        if ($key == 0)
+                            continue;
+                        $sum -= $num;
+                    };
+                    break;
+                case '*':
+                    $sum = $nums[0];
+                    print_r($nums);
+                    foreach ($nums as $key => $num) {
+                        if ($key == 0)
+                            continue;
+                        $sum *= $num;
+                    };
+                    break;
+                case '/':
+                    $sum = $nums[0];
+                    foreach ($nums as $key => $num) {
+                        if ($key == 0) {
+                            continue;
+                        } elseif ($num == 0) {
+                            echo '<b>Делить на ноль? Извольте!</b>';
+                            die();
+                        } else {
+                            $sum /= $num;
+                        }
+                    };
+                    break;
+                default:
+                    $sum = '<b>Такая математика не для нас!</b>';
+
+                }
+    echo "<b>Результат: $sum</b>";
+};
+////////////////////////////////////////////////////////////
+function task3(){
+    $nums = func_get_args();
+    $math = array_shift($nums);
+
+    //дальше задача сводится к предыдущей, но тут я обнаружил array_shift()
+    //и решил сделать ее немного подругому :)
+
+    foreach($nums as $num) {
+        if (!is_numeric($num)) {
+            echo '<b>Результат: нужны только цифры нам, о юный падаван!</b>';
+            die();
+        }
+    };
+            switch ($math) {
+                case '+':
+                    $sum = 0;
+                    foreach ($nums as $num) {
+                        $sum += $num;
+                    };
+                    break;
+                case '-':
+                    $sum = array_shift($nums);
+                    foreach ($nums as $num) {
+                        $sum -= $num;
+                    };
+                    break;
+                case '*':
+                    $sum = array_shift($nums);
+                    foreach ($nums as $key => $num) {
+                        $sum *= $num;
+                    };
+                    break;
+                case '/':
+                    $sum = array_shift($nums);
+                    foreach ($nums as $key => $num) {
+                        if ($num == 0) {
+                            echo '<b>Делить на ноль? Извольте!</b>';
+                            die();
+                        } else {
+                            $sum /= $num;
+                        }
+                    };
+                    break;
+                default:
+                    $sum = '<b>Такая математика не для нас!</b>';
+            }
+    echo "<b>Результат: $sum</b>";
+};
+
+//////////////////////////////////////////////////
+
+function task4($x,$y){
+
+    if ((!is_integer($x)||!is_integer($y))||($x>20||$y>20)||($x<1||$y<1)) {
+        echo '<b>Результат: нужны только целые числа нам, о юный падаван! Желательно от 1 до 20</b>';
+        die();
+    }
+    echo'<table border=1>';
+    for($i = 1; $i <= abs($x); $i++) { //если ввели отрицательные, то возьмем по модулю
+        echo'<tr>';
+        for($j = 1; $j <= abs($y); $j++)
+
+            echo '<td>' .$i*$j. '</td>';
+
+        echo'</tr>';
+    }
+    echo'</table>';
+
+};
+
+//////////////////////////////////////////////////
+function task5_1($str1){
+    $str1 = mb_strtolower($str1, "UTF-8");
+    $str1 = str_replace(" ","",$str1);
+
+    $str2 = "";
+    for($i = mb_strlen($str1, "UTF-8"); $i >= 0; $i--) {
+        $str2 .= mb_substr($str1, $i, 1, "UTF-8");
+    }
+    if($str1 != $str2){
+        return FALSE;
+    } else {
+        return TRUE;
+    }
+};
+
+function task5_2($pol,$str1){
+    if ($pol == TRUE) {
+        echo 'Строка  - "' . $str1 . '" - палиндром';
+        } else {
+        echo 'Строка  - "' . $str1 . '" - не палиндром';
+
+    }
+};
+///////////////////////////////////////////////////
+function task6($format,$date){
+
+    $date1 = date($format);
+    $date2 = strtotime($date);
+    echo 'Текущая дата: ' . $date1 . '<br />Время в unixtime: ' . $date2;
+
+};
+///////////////////////////////////////////////////
+function task7($str1,$str2){
+    $str1 = str_replace("К", "к", $str1);
+    $str2 = str_replace("Две", "Три", $str2);
+    echo $str1 . '<br />' . $str2;
+};
+////////////////////////////////////////////////////
+function task8_1(){
+    echo '<pre>';
+    echo '   
+             OOOOOOOOOOO
+         OOOOOOOOOOOOOOOOOOO
+      OOOOOO  OOOOOOOOO  OOOOOO
+    OOOOOO      OOOOO      OOOOOO
+  OOOOOOOO  #   OOOOO  #   OOOOOOOO
+ OOOOOOOOOO    OOOOOOO    OOOOOOOOOO
+OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO
+ OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO
+  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO
+    OOOOO   OOOOOOOOOOOOOOO   OOOO
+      OOOOOO   OOOOOOOOO   OOOOOO
+         OOOOOO         OOOOOO
+             OOOOOOOOOOOO';
+};
+function task8_2($rx_str){
+    preg_match('/[\d]+/',$rx_str,$speed);
+    preg_match('/:\)/',$rx_str,$smile);
+    if ($smile) {
+        task8_1();
+        //echo 'Сеть есть!';
+    } elseif ($speed[0]>1000) {
+        echo 'Сеть есть!';
+        };
+};
+//////////////////////////////////////////////////////
+function task9($fname){
+    print_r(file_get_contents($fname));
+};
+
+function task10($s){
+    echo 'Создаем файл и поместим в него строку<br />';
+    file_put_contents('anothertest.txt',$s);
+    echo 'Прочитаем содержимое:<br />';
+    print_r(file_get_contents('anothertest.txt'));
+};
+?>
---
+++ hw2/index.php
@@ -0,0 +1,125 @@
+﻿<?php
+require ('functions.php');
+
+echo 'Задание #1 <br /><br />';
+
+$arr = array('Вася','Петя','Саша');
+
+echo 'Передаем параметр true:<br /><br />';
+task1($arr,true);
+echo '<br /><br />';
+
+echo 'А теперь без него:<br />';
+
+task1($arr);
+
+echo '<hr>';
+/////////////////////////////////////////////////
+echo 'Задание #2 <br />';
+
+echo '<p>В первое поле вводим цифры через запятую, во второе знак арифметического действия.<br />
+Можно проверить некоректный ввод (не цифры, не арифметические знаки, деление на ноль)
+</p><br />';
+
+
+echo '<form name="task2" action="">
+        <input name="nums" "text">
+        <input name="math" "text">
+        <input id=task2 type="submit">
+        </form>';
+
+
+if (isset($_REQUEST['nums'])&&isset($_REQUEST['math'])) {
+    $nums = explode(",", $_REQUEST['nums']);
+    $math = $_REQUEST['math'];
+    task2($nums,$math);
+};
+
+echo '<hr>';
+/////////////////////////////////////////////////
+echo 'Задание #3 <br />';
+echo 'Передадим в функцию +, 1, 8, 10, 3.5<br />';
+
+    task3('+', 1, 8, 10, 3.5);
+
+echo '<hr>';
+/////////////////////////////////////////////////
+echo 'Задание #4 <br />';
+
+echo '<p>Введите 2 целых числа от 1 до 20</p><br />';
+
+
+echo '<form name="task4" action="">
+        <input name="x" type="number">
+        <input name="y" type="number">
+        <input id=task4 type="submit">
+        </form>';
+if (isset($_REQUEST['x'])&&isset($_REQUEST['y'])) {
+    $x = intval($_REQUEST['x']);
+    $y = intval($_REQUEST['y']);
+    task4($x,$y);
+};
+echo '<hr>';
+/////////////////////////////////////////////////
+echo 'Задание #5 <br />';
+
+echo '<p>Проверим строку на палиндром:</p><br />';
+echo '<form name="task5" action="">
+        <input name="str" type="text">
+        <input id=task5 type="submit">
+        </form>';
+
+if (isset($_REQUEST['str'])) {
+    $str1 = $_REQUEST['str'];
+    $pol = task5_1($str1);
+    task5_2($pol, $str1);
+};
+
+echo '<hr>';
+/////////////////////////////////////////////////
+echo 'Задание #6 <br />';
+
+$format = 'd.m.Y H:i';
+$date = '24.02.2016 00:00:00';
+task6($format,$date);
+
+echo '<hr>';
+/////////////////////////////////////////////////
+echo 'Задание #7 <br />';
+
+$str1 = 'Карл у Клары украл Кораллы';
+$str2 = 'Две бутылки лимонада';
+
+task7($str1,$str2);
+
+echo '<hr>';
+/////////////////////////////////////////////////
+echo 'Задание #8 <br />';
+
+$rx_str = 'RX packets:1001 errors:0 dropped:0 overruns:0 frame:0.';
+
+task8_2($rx_str);
+
+echo '<hr>';
+/////////////////////////////////////////////////
+echo 'Задание #9 <br />';
+
+echo '<p>Введите имя файла (test.txt)</p><br />';
+echo '<form name="task9" action="">
+        <input name="fname" type="text">
+        <input id=task9 type="submit">
+        </form>';
+
+if (isset($_REQUEST['fname'])) {
+    $fname = $_REQUEST['fname'];
+    task9($fname);
+};
+
+echo '<hr>';
+/////////////////////////////////////////////////
+echo 'Задание #10 <br />';
+
+$s = 'Hello again!';
+task10($s);
+
+?>
